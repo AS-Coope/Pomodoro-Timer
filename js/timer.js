1dfe0d61092968cdc2abacfb4b6fa7caf723df87
AS-Coope <deltazirconium@gmail.com>
2023-12-18 09:43:32 -0500

    Create a rough plan of webpage functionality

--- js/timer.js
+++ js/timer.js
@@ -1,1 +1,16 @@
-
+// variable initialization
+// 1. Click play btn
+// 2. Start timer
+// 3. Play button icon converts to pause button icon
+// 4. Reset button is always beside play button
+// 4a. It should not do anything to the timer
+//     once the timer has not been started
+//     (functionality that can be added later is that
+//     the button can be greyed out or disabled once the timer
+//     is not started)
+// 5. Timer should tick down to the end or until the pause btn is hit
+// 5a. If pause button is hit, then the pause button icon changes to
+//     a play button icon and the timer stops ticking down.
+//     Reset button will still be active
+// 5b. If pause button is not hit, then the timer will tick down
+//     to zero minutes, zero seconds.
