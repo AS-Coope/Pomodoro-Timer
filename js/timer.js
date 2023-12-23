591e02bae9a9604c4685e816a0c302e11692d37e
AS-Coope <deltazirconium@gmail.com>
2023-12-23 10:31:15 -0500

    Implement logic for calculating minute and seconds per second

--- js/timer.js
+++ js/timer.js
@@ -3,16 +3,19 @@ const playBtn = document.getElementById("play-btn");
 const resetBtn = document.getElementById("reset-btn");
 const timerScrn = document.querySelector(".timer-screen");
 const ONE_SECOND_IN_MILLISECONDS = 1000;
-const TIMER_MAX_VALUE = 10 * ONE_SECOND_IN_MILLISECONDS; // 10 seconds
+const ONE_MINUTE_IN_MILLISECONDS = 60 * ONE_SECOND_IN_MILLISECONDS;
+const ONE_MINUTE_IN_SECONDS = 60;
+const TOTAL_NUM_OF_MINUTES = 2;
+const TIMER_MAX_VALUE = TOTAL_NUM_OF_MINUTES * ONE_MINUTE_IN_MILLISECONDS; // 10 seconds
 let isTicking = false;
-let currentTimerValue = TIMER_MAX_VALUE;
+let currentMaxTimeInMs = TIMER_MAX_VALUE;
 let intervalId;
 
 initializePage();
 // 1. Click play btn
 playBtn.addEventListener('click', () => {
 
-    if (currentTimerValue === 0) {
+    if (currentMaxTimeInMs === 0) {
         // here, timer should be reset and button changed back to play button
         console.log("Timer is complete");
     } else {
@@ -20,10 +23,14 @@ playBtn.addEventListener('click', () => {
         // 2. Start timer
         if (isTicking === true) {
             intervalId = setInterval(() => {
-                currentTimerValue = currentTimerValue - ONE_SECOND_IN_MILLISECONDS;
-                setTimerText(timerScrn, currentTimerValue);
-                console.log(`${currentTimerValue} seconds`);
-                if (currentTimerValue === 0) {
+                currentMaxTimeInMs = currentMaxTimeInMs - ONE_SECOND_IN_MILLISECONDS;
+
+                const seconds = Math.floor((currentMaxTimeInMs / ONE_SECOND_IN_MILLISECONDS) % 60);
+                const minutes = Math.floor(currentMaxTimeInMs / (ONE_SECOND_IN_MILLISECONDS * ONE_MINUTE_IN_SECONDS));
+                setTimerText(timerScrn, `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`);
+                console.log(`${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`);
+
+                if (currentMaxTimeInMs === 0) {
                     console.log("Timer Done!");
                     clearInterval(intervalId);
                     // 3. Play button icon converts to pause button icon
@@ -38,16 +45,16 @@ playBtn.addEventListener('click', () => {
 });
 
 resetBtn.addEventListener('click', () => {
-    // check if currentTimerValue = TIMER_MAX_VALUE (no resetting required since timer isn't started)
-    if (currentTimerValue === TIMER_MAX_VALUE) {
+    // check if currentMaxTimeInMs = TIMER_MAX_VALUE (no resetting required since timer isn't started)
+    if (currentMaxTimeInMs === TIMER_MAX_VALUE) {
         console.log("Nothing doing, timer hasn't started")
     }
     else if (intervalId === undefined) {
         console.log("Timer hasn't started to count down yet");
     } else {
         clearInterval(intervalId);
-        currentTimerValue = TIMER_MAX_VALUE;
-        setTimerText(timerScrn, currentTimerValue);
+        currentMaxTimeInMs = TIMER_MAX_VALUE;
+        setTimerText(timerScrn, currentMaxTimeInMs);
         if (isTicking === true) {
             changePlayBtn(); // the button must not still be showing that the timer is playing/ticking
         }
@@ -66,17 +73,5 @@ function setTimerText(elem, timerValue) {
 }
 
 function initializePage() {
-    setTimerText(timerScrn, currentTimerValue);
+    setTimerText(timerScrn, currentMaxTimeInMs);
 }
-// 4. Reset button is always beside play button
-// 4a. It should not do anything to the timer
-//     once the timer has not been started
-//     (functionality that can be added later is that
-//     the button can be greyed out or disabled once the timer
-//     is not started)
-// 5. Timer should tick down to the end or until the pause btn is hit
-// 5a. If pause button is hit, then the pause button icon changes to
-//     a play button icon and the timer stops ticking down.
-//     Reset button will still be active
-// 5b. If pause button is not hit, then the timer will tick down
-//     to zero minutes, zero seconds.
